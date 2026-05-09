181de45b0dbd7f4dcf550c98b999c790861587a1
Chiara Protopapa <tino.pino1207@gmail.com>
2026-05-09 09:46:39 +0200

    feat: Implement IntegertToRoman convert() method

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,7 +7,32 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number){
-    
-    return null;
-}
+	//number limits
+	if (number < 1 || number > 1000) {
+            throw new IllegalArgumentException(
+                "Number must be between 1 and 1000"
+            );
+        }
+	// Arrays of arabic values and their roman equivalents
+        int[] values = {1, 4, 5, 9, 10, 40, 50, 90,
+                        100, 400, 500, 900, 1000};
+        String[] romanNum = {"I", "IV", "V", "IX", "X", "XL",
+                             "L", "XC", "C", "CD", "D", "CM", "M"};
+        String romanS = ""; //output string
+	while (number > 0) {
+            int max = 0;
+	    //finds the largest Roman numeral value
+	    // less or equal to the current number
+            for (int i = 0; i < values.length; i++) {
+                if (number >= values[i]) {
+                    max = i;
+                }
+            }
+	    //add the corresponding Roman symbol
+            romanS += romanNum[max];
+	    //subtracts that value from the number
+            number -= values[max];
+        }
+        return romanS;
+    }
 }
